167dd71e30801dea39de17bcaa713911190feb0b
Alexandre Girard <git@alexgirard.com>
2024-09-21 17:05:03 +0700

    emacs: disable openwith

--- dotfiles/doom/config.el
+++ dotfiles/doom/config.el
@@ -138,30 +138,30 @@
 ;; Ten Org-mode Extensions You Need to Install!
 ;; https://youtu.be/5Y75ICmzQV0
 ;;
-(when (require 'openwith nil 'noerror)
-  (setq openwith-associations
-        (list
-         (list (openwith-make-extension-regexp
-                '("mpg" "mpeg" "mp3" "mp4"
-                  "avi" "wmv" "wav" "mov" "flv"
-                  "ogm" "ogg" "mkv"))
-               "vlc"
-               '(file))
-         (list (openwith-make-extension-regexp
-                '("xbm" "pbm" "pgm" "ppm" "pnm"
-                  "png" "gif" "bmp" "tif" "jpeg" "jpg"))
-               "geeqie"
-               '(file))
-         (list (openwith-make-extension-regexp
-                '("doc" "xls" "ppt" "odt" "ods" "odg" "odp"))
-               "libreoffice"
-               '(file))
-         '("\\.lyx" "lyx" (file))
-         '("\\.chm" "kchmviewer" (file))
-         (list (openwith-make-extension-regexp
-                '("pdf" "ps" "ps.gz" "dvi"))
-               "okular"
-               '(file))
-         ))
-  (openwith-mode 1))
-
+;; (when (require 'openwith nil 'noerror)
+;;   (setq openwith-associations
+;;         (list
+;;          (list (openwith-make-extension-regexp
+;;                 '("mpg" "mpeg" "mp3" "mp4"
+;;                   "avi" "wmv" "wav" "mov" "flv"
+;;                   "ogm" "ogg" "mkv"))
+;;                "vlc"
+;;                '(file))
+;;          (list (openwith-make-extension-regexp
+;;                 '("xbm" "pbm" "pgm" "ppm" "pnm"
+;;                   "png" "gif" "bmp" "tif" "jpeg" "jpg"))
+;;                "geeqie"
+;;                '(file))
+;;          (list (openwith-make-extension-regexp
+;;                 '("doc" "xls" "ppt" "odt" "ods" "odg" "odp"))
+;;                "libreoffice"
+;;                '(file))
+;;          '("\\.lyx" "lyx" (file))
+;;          '("\\.chm" "kchmviewer" (file))
+;;          (list (openwith-make-extension-regexp
+;;                 '("pdf" "ps" "ps.gz" "dvi"))
+;;                "okular"
+;;                '(file))
+;;          ))
+;;   (openwith-mode 1))
+;;
