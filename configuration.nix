7f2bb8ee969e9ee9686973a6a39d55bf300d4c52
Alexandre Girard <git@alexgirard.com>
2024-10-16 19:18:15 +0700

    feat: programs.steam

--- configuration.nix
+++ configuration.nix
@@ -155,6 +155,12 @@
   #   enable = true;
   #   enableSSHSupport = true;
   # };
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
 
   # List services that you want to enable:
 
