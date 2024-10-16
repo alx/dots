8d6204c1de1db85f8110a6d41cfc72623664841e
Alexandre Girard <git@alexgirard.com>
2024-10-16 19:21:03 +0700

    fix: doom font size

--- dotfiles/doom/config.el
+++ dotfiles/doom/config.el
@@ -33,11 +33,11 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-dark+)
-(setq doom-font                 (font-spec :family "Monaspace Neon"  :size 26 :weight 'light)
+(setq doom-font                 (font-spec :family "Monaspace Neon"  :size 20 :weight 'light)
       doom-symbol-font          doom-font
       doom-big-font-increment   4
-      doom-variable-pitch-font  (font-spec :family "Monaspace Argon" :size 28 :weight 'light)
-      doom-serif-font           (font-spec :family "Monaspace Neon"  :size 26 :weight 'light))
+      doom-variable-pitch-font  (font-spec :family "Monaspace Argon" :size 22 :weight 'light)
+      doom-serif-font           (font-spec :family "Monaspace Neon"  :size 20 :weight 'light))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
