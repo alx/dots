b681f91be51bfe937d419b38d14a9796eae7680a
Alexandre Girard <git@alexgirard.com>
2024-10-28 07:37:03 +0700

    fix: add dotfiles

---
+++ dotfiles/bin/screenshot2org.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+_alarm() {
+  ( \speaker-test --frequency $1 --test sine --scale 5 )&
+  pid=$!
+  \sleep 0.${2}s
+  \kill -9 $pid
+}
+
+_alarm 800 150
+flameshot full
+
+source /home/alx/org/.venv/bin/activate
+python3 /home/alx/org/bin/process_inbox_screenshots.py
+_alarm 1000 200
