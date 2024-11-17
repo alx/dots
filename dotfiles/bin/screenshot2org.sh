83c712b797aa4bb2c3ab78630e1a822e5716c9fd
Alexandre Girard Davila <git@alexgirard.com>
2024-11-17 08:59:17 +0700

    fix: review screenshot script

--- dotfiles/bin/screenshot2org.sh
+++ dotfiles/bin/screenshot2org.sh
@@ -6,24 +6,19 @@ _alarm() {
   \sleep 0.${2}s
   \kill -9 $pid
 }
-
-screenshot_root_path=/home/alx/org/inbox/screenshots/
-screenshot_path="$screenshot_root_path"$(date +%Y-%m-%d_%H-%M-%S).png
-
-browser_url=$(./get_url.py)
-
-echo $screenshot_path
-echo $browser_url
+echo "start" >> /home/alx/screenshot.log
 
 # paste current browser url to clipboard
+browser_url=$(/home/alx/bin/get_url.py)
+echo $browser_url >> /home/alx/screenshot.log
 echo $browser_url | xclip -sel clip
 
 _alarm 800 150
+screenshot_root_path=/home/alx/org/inbox/screenshots/
+screenshot_path="$screenshot_root_path"$(date +%Y-%m-%d_%H-%M-%S).png
 flameshot full -p $screenshot_path
 
 source /home/alx/org/.venv/bin/activate
-python3 /home/alx/org/bin/process_inbox_screenshots.py \
-  -u $(./get_url.py) \
-  -s $screenshot_path
+python3 /home/alx/org/bin/process_inbox_screenshots.py -u $browser_url -s $screenshot_path 2&>1
 
 _alarm 1000 200
