5254c97812018760b4a6a058af17f550ff9d42c5
Alexandre Girard Davila <git@alexgirard.com>
2024-11-25 07:40:02 +0700

    fix: comment gjg/gptel-build-directives until fix

--- dotfiles/doom/config.el
+++ dotfiles/doom/config.el
@@ -151,36 +151,36 @@
 
 ;;     old block header: #+begin_src emacs-lisp :results none :tangle gptel-build-directives.el :comments org
 
-(defun gjg/parse-prompt-file (prompt-file)
-  "Parse a single prompt file and return its description and content."
-  (with-temp-buffer
-    (insert-file-contents prompt-file)
-    (let ((prompt-description "NO DESCRIPTION"))
-      ;; nab the description - single-line descriptions only!
-      (goto-char (point-min))
-      (when (re-search-forward "#\\+description: \\(.*?\\) *--> *$" nil t)
-        (setq prompt-description (match-string 1)))
-      ;; remove all comments
-      (delete-matching-lines "^ *<!--" (point-min) (point-max))
-      ;; remove leading blank lines
-      (goto-char (point-min))
-      (while (and (looking-at "^$") (not (eobp)))
-        (delete-char 1))
-      ;; return the description and content
-      (list prompt-description (buffer-substring-no-properties (point-min) (point-max))))))
-
-(defun gjg/gptel-build-directives (promptdir)
-  "Build `gptel-directives' from Markdown files in PROMPTDIR."
-  (let* ((prompt-files (directory-files promptdir t "md$")))
-    (mapcar (lambda (prompt-file)
-              (let ((parsed-prompt (gjg/parse-prompt-file prompt-file)))
-                (cons (intern (f-base prompt-file))  ; gptel-directives key
-                      (nth 1 parsed-prompt))))       ; prompt content
-            prompt-files)))
+;;(defun gjg/parse-prompt-file (prompt-file)
+;;  "Parse a single prompt file and return its description and content."
+;;  (with-temp-buffer
+;;    (insert-file-contents prompt-file)
+;;    (let ((prompt-description "NO DESCRIPTION"))
+;;      ;; nab the description - single-line descriptions only!
+;;      (goto-char (point-min))
+;;      (when (re-search-forward "#\\+description: \\(.*?\\) *--> *$" nil t)
+;;        (setq prompt-description (match-string 1)))
+;;      ;; remove all comments
+;;      (delete-matching-lines "^ *<!--" (point-min) (point-max))
+;;      ;; remove leading blank lines
+;;      (goto-char (point-min))
+;;      (while (and (looking-at "^$") (not (eobp)))
+;;        (delete-char 1))
+;;      ;; return the description and content
+;;      (list prompt-description (buffer-substring-no-properties (point-min) (point-max))))))
+;;
+;;(defun gjg/gptel-build-directives (promptdir)
+;;  "Build `gptel-directives' from Markdown files in PROMPTDIR."
+;;  (let* ((prompt-files (directory-files promptdir t "md$")))
+;;    (mapcar (lambda (prompt-file)
+;;              (let ((parsed-prompt (gjg/parse-prompt-file prompt-file)))
+;;                (cons (intern (f-base prompt-file))  ; gptel-directives key
+;;                      (nth 1 parsed-prompt))))       ; prompt content
+;;            prompt-files)))
 
 (use-package! gptel
   :init
-  (setq gptel-directives (gjg/gptel-build-directives "~/code/AIPIHKAL/system-prompts/"))
+  ;;(setq gptel-directives (gjg/gptel-build-directives "~/code/AIPIHKAL/system-prompts/"))
   :config
   (setq
    gptel-model 'claude-3-opus-20240229 ;  "claude-3-opus-20240229" also available
